2976dab7b967813c7785afb64ddff2adba090bf0
William Sanchez <183452295+WilliamSanchezR@users.noreply.github.com~>
2025-09-11 16:07:32 -0500

    Cree la seccion de reviews con sus respectivos estilos

--- js/main.js
+++ js/main.js
@@ -25,3 +25,29 @@
     });
     });
 
+
+    $(document).ready(function(){
+    $('.slider-reviews').slick({
+        slidesToShow: 3,   // muestras 4 columnas al mismo tiempo
+        slidesToScroll: 1, // avanzas de a 1
+        infinite: true,    // loop infinito
+        arrows: true,      // flechas
+        dots: false,        // bolitas de navegación
+        autoplay: true,
+        speed: 300,
+        responsive: [
+        {
+            breakpoint: 992, // tablets y pantallas medianas
+            settings: {
+            slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 576, // móviles
+            settings: {
+            slidesToShow: 1
+            }
+        }
+        ]
+    });
+    });
